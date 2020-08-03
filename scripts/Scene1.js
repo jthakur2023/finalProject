08cda490d584c6101a8543538de4d246952148ee
Michael Tan <michaeltan32@gmail.com>
2020-08-03 10:28:52 -0400

    Updated Scene 1 with preloading images. Updated naming to seperate rain and rain2 clouds.

--- scripts/Scene1.js
+++ scripts/Scene1.js
@@ -4,7 +4,16 @@ class Scene1 extends Phaser.Scene{
     }
 
     preload() {
-      this.load.image('background', "assets/images/landscape scaled.png");
+      // Background Image
+      this.load.image("background", "assets/images/landscape scaled.png");
+      // Player Image
+      this.load.image("player", "assets/images/kite.png");
+      // Miscellaneous Images
+      this.load.image("cloud", "assets/images/cloud.png");
+      this.load.image("rain", "assets/images/rain cloud.png");
+      this.load.image("lightning", "assets/images/ligning cloud.png");
+      this.load.image("tree", "assets/images/tree.png");
+      this.load.image("cow", "assets/images/cow.png");
     }
     
     create() {
