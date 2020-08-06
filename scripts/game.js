1fe57e349caff895b4367b778fce97e393d1df01
jthakur2023 <thakurj2000@gmail.com>
2020-08-05 21:36:35 -0400

    added geocoderand reverse geocoding

--- scripts/game.js
+++ scripts/game.js
@@ -19,6 +19,46 @@ let config = {
             debug: false
         }
     }
+    
 }
+const successCallback = (position) => {
+    console.log(position.coords.latitude, position.coords.longitude);
+    //document.getElementById(`location`).innerHTML = `Latitude: `+ position.coords.latitude + ` Longitude: `+position.coords.longitude;
+    getCity(position);
+  };
+  
+  
+  const errorCallback = (error) => {
+    console.error(error);
+  };
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback,{
+    enableHighAccuracy: true,
+    timeout: 5000
+  });
+  
+  
+  function getCity(position) { 
+    let xhr = new XMLHttpRequest(); 
+    let lat = position.coords.latitude;
+    let lng = position.coords.longitude; 
+  
+  
+    xhr.open(`GET`, `https://us1.locationiq.com/v1/reverse.php?key=69771d303c7ec1&lat=` + 
+    lat + `&lon=` + lng + `&format=json`, true); 
+    xhr.send(); 
+    xhr.onreadystatechange = processRequest; 
+    xhr.addEventListener(`readystatechange`, processRequest, false); 
+  
+    function processRequest(e) { 
+      if (xhr.readyState == 4 && xhr.status == 200) { 
+        let response = JSON.parse(xhr.responseText); 
+        let city = response.address.city; 
+        console.log(city); 
+        //document.getElementById(`city`).innerHTML = `City: `+ city;
+        return; 
+      } 
+    } 
+}
+
 
 let game = new Phaser.Game(config);
